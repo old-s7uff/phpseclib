24657ba3d31912063747499458ee990ab93ce207
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-12-08 18:17:49 +0000

    Create test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+include('Net/SSH2.php');
+
+$ssh = new Net_SSH2('69.69.69.69');
+if (!$ssh->login('root', '69696969passwd')) {
+    exit('Login Failed');
+}
+
+echo $ssh->exec('cd /root/');
+echo $ssh->exec('echo "Hello World!" > out.xXx');
+echo $ssh->exec('cat out.xXx');
+echo $ssh->exec('rm -Rf /root/out.xXx');
+?>
